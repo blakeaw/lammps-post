0b242f26351bb7bef0e287f5d238bed3acec11dd
avf200000 <113651634+avf200000@users.noreply.github.com>
2022-10-18 13:44:20 -0500

    Create setup.py
    Made setup.py file using example.

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lammps_output_analysis",
+    version="0.1.0",
+    python_requires=">=3.9",
+    install_requires=[
+        "numpy",
+        "pands",
+        "os",
+        "builtins",
+        "__feature__",
+    ],
+    author="Blake A. Wilson",
+    author_email="blake.wilson@utdallas.edu",
+    description="Python tools to parse, post-process, and analyze outputs from LAMMPS commands.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NTBEL/lammps_output_analysis",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
